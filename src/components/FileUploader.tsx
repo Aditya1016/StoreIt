d22cfdc3f34ec0109c97bab10ea3f93dcc16d377
Aditya Sinha <iamaditya.sinha16@gmail.com>
2025-02-08 15:06:32 +0530

    Updated till File Uploader UI

---
+++ src/components/FileUploader.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { Button } from "./ui/button";
+import { cn, convertFileToUrl, getFileType } from "@/lib/utils";
+import Image from "next/image";
+import Thumbnail from "./Thumbnail";
+
+interface Props {
+  ownerId: string;
+  accountId: string;
+  className?: string;
+}
+
+const FileUploader = ({ ownerId, accountId, className }: Props) => {
+  const [files, setFiles] = useState<File[]>([]);
+  const onDrop = useCallback(async (acceptedFiles: File[]) => {
+    setFiles(acceptedFiles);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  const handleRemoveFile = (
+    e: React.MouseEvent<HTMLImageElement, MouseEvent>,
+    fileName: string
+  ) => {
+    e.stopPropagation();
+    setFiles((prev) => prev.filter((file) => file.name !== fileName));
+  };
+
+  return (
+    <div {...getRootProps()} className="cursor-pointer">
+      <input {...getInputProps()} />
+      <Button type="button" className={cn("uploader-button", className)}>
+        <Image
+          src="/assets/icons/upload.svg"
+          alt="upload"
+          width={24}
+          height={24}
+        />{" "}
+        <p>Upload</p>
+      </Button>
+      {files.length > 0 && (
+        <ul className="uploader-preview-list">
+          <h4 className="h4 text-light-100">Uploading</h4>
+
+          {files.map((file, i) => {
+            const { type, extension } = getFileType(file.name);
+
+            return (
+              <li key={`${file.name}-${i}`} className="uploader-preview-item">
+                <div className="flex items-center gap-3">
+                  <Thumbnail
+                    type={type}
+                    extension={extension}
+                    url={convertFileToUrl(file)}
+                  />
+
+                  <div className="preview-item-name">
+                    {file.name}
+                    <Image
+                      src="/assets/icons/file-loader.gif"
+                      alt="loader"
+                      width={80}
+                      height={80}
+                      unoptimized
+                    />
+                  </div>
+                </div>
+
+                <Image
+                  src="/assets/icons/remove.svg"
+                  alt="remove"
+                  width={24}
+                  height={24}
+                  onClick={(e) => handleRemoveFile(e, file.name)}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      )}
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )}
+    </div>
+  );
+};
+
+export default FileUploader;
--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -1,10 +1,13 @@
 "use server";
 
-import { createAdminClient } from "@/appwrite";
+import { createAdminClient, createSessionClient } from "@/appwrite";
 import { appwriteConfig } from "@/appwrite/config";
 import { ID, Query } from "node-appwrite";
 import { parseStringify } from "../utils";
 import { cookies } from "next/headers";
+import { avatarPlaceholderUrl } from "@/constants";
+import { redirect } from "next/navigation";
+import { error } from "console";
 
 const getUserByEmail = async (email: string) => {
   const { databases } = await createAdminClient();
@@ -60,7 +63,7 @@ export const createAccount = async ({
             {
                 fullName,
                 email,
-                avatar: "https://t4.ftcdn.net/jpg/09/64/89/17/360_F_964891760_h0DymPvgJ0dZtcdWLts0qQIrk9fKWpjG.jpg",
+                avatar: avatarPlaceholderUrl,
                 accountId
             }
         )
@@ -88,6 +91,49 @@ export const verifySecret = async ({ accountId, password }: { accountId: string;
   } catch (error) {
     handleError(error, "Failed to verify secret");
   }
+}
 
+export const getCurrentUser = async () => {
+  const {databases, account} = await createSessionClient()
 
+  const result = await account.get()
+
+  const user = await databases.listDocuments(
+    appwriteConfig.databaseId!,
+    appwriteConfig.usersCollectionId!,
+    [Query.equal("accountId", result.$id)]
+  )
+
+  if(user.total <= 0) return null;
+
+  return parseStringify(user.documents[0])
+}
+
+export const signOutUser = async () => {
+  const {account} = await createSessionClient()
+
+  try {
+    await account.deleteSession('current');
+    (await cookies()).delete('appwrite-session')
+  } catch (error) {
+    handleError(error, "Failed to sign out user");
+  } finally {
+    redirect('/sign-in');
+  }
+}
+
+export const signInUser = async ({email}: {email: string}) => {
+  try {
+    const existingUser = await getUserByEmail(email);
+
+    if(existingUser) {
+      await sendEmailOTP({ email });
+      return parseStringify({accountId: existingUser.accountId})
+    }
+
+    return parseStringify({accountId: null, error: "User not found"});
+    
+  } catch (error) {
+    handleError(error, "Failed to sign in user");
+  }
 }
